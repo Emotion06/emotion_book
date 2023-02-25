eb0e9a6af8ba7cd6e66f9a58425a6af3e15d9b2f
Emotion <111930499+Emotion06@users.noreply.github.com>
2023-02-25 21:42:24 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # emotion_book
-This is a simple book script for RedM!
+
+> This is a simple book script for RedM!
+
+## REQUIREMENTS
+- VORP Core
+- VORP Inventory
+
+## HOW TO USE THE SCRIPT
+- Add the file to your resource file
+- Add ensure 'emotion_book' to your resources.cfg
+- Run the SQL or add the items manually in the database
+- Add the pictures into your vorp_inventory/html/img/items folder
+
+## HOW CAN YOU DESIGN YOUR OWN BOOK
+
+### Pages Info:
+You need to make each pages with any photo editor !
+The default page size is: 691x900
+All the pages config are in the file (emotion_book\html\client.js) lines 115 +
+Right now by default all the pages link are stored on my discord.
+You need to change all the discord link to the pages you going to use for your server.
+You can store the page on a private discord channel or on any website.
+
+### Page on Discord:
+Upload the pages to discord in a private channel
+Put the mouse on the picture
+Right clic and copy the link
+
+### Change the Table of contents:
+The line 121 (/* 2 index */) is the table of contents.
+The order of all pages is written like this: /* 1 cover */  /* 2 index */  /* 3 ad */ and /* 4 */ until the last pages...
+On the line: 122 content: if you want to change the page it open when you clic on the table of contents you need to change the (Index) for the page order.
+There is some info that can be change in the stylesheet.css if you change the name of the table of contents
---
+++ html/client.js
@@ -0,0 +1,186 @@
+var data
+var book = document.getElementsByClassName("flipbook")[0]
+var frontimage = document.getElementById("frontimage")
+var midimage = document.getElementById("midimage")
+var backimage = document.getElementById("backimage")
+var inited = false
+var editable = false
+var currentEditingPageIndex
+var _editor
+var settings
+var onClose
+var bookIsOpen = false
+
+document.getElementById("closesettingsbutton").onclick = function() {
+    SetClass("w_settings","off")
+}
+
+function StartBook(args) {
+    if( args == null ) { return false }
+    if( args.data == null ) {return false}
+    d = args.data
+    edit = !!args.edit
+    onClose = args.onClose
+
+    SetClass("w_book","off",false)
+    SetClass("w_content","off",false)
+
+    if( !inited ) {
+        inited = true
+        $('.flipbook').turn({
+            width:922*1.5,
+            height:600*1.5,
+            elevation: 50,
+            gradients: true,
+            autoCenter: true
+        });
+    }
+
+    editable = edit
+    data = d
+    frontimage.value = data.frontImage || ""; frontimage.onchange = function() { data.frontImage = frontimage.value }
+    midimage.value = data.midImage || ""; midimage.onchange = function() { data.midimage = midimage.value }
+    backimage.value = data.backImage || ""; backimage.onchange = function() { data.backimage = backimage.value }
+
+    if( data == null || data == "" || data.pages == null || data.pages.length == 0 ) {
+        $(".flipbook").turn("addPage", CreatePageElement(editable))
+        $(".flipbook").turn("addPage", CreatePageElement(editable))
+        $(".flipbook").turn("addPage", CreatePageElement(editable))
+    } else {
+        BuildPages()
+        $(".flipbook").turn("page",1);
+    }
+    $(".flipbook").bind("turned", function(event, page, view) {
+        document.getElementsByClassName("w_flip")[0].classList.toggle("off",true)
+    });
+}
+
+function StopBook() {
+    SetClass("w_book","off",true)
+    SetClass("w_content","off",true)
+}
+
+function BuildPages() {
+    var pageCount = $(".flipbook").turn("pages")
+    for( var i = pageCount; i > 0; i--) {
+        if($(".flipbook").turn("hasPage",i) == false ) { continue}
+        $(".flipbook").turn("removePage", i);
+    }
+    for( var i = 0; i < data.pages.length; i++ ) {
+        $(".flipbook").turn("addPage", CreatePageElement(editable,data.pages[i]))
+    }
+}
+
+function CreatePageElement(editable,data) {
+    var element = document.createElement("div")
+    element.classList.add("w_bookpage")
+    element.id = "bookpage" + ($(".flipbook").turn("pages")+1)
+    if( data != null && data.background != null ) {
+        element.style.backgroundImage = "url('"+data.background+"')"
+    }
+    if( data != null && data.backgroundColor != null ) {
+        element.style.backgroundColor = data.backgroundColor
+    }
+    if( data != null && data.color != null ) {
+        element.style.color = data.color
+    }
+    var cont = document.createElement("div")
+    cont.id = "pagecontent" + ($(".flipbook").turn("pages")+1)
+    cont.classList.add("w_pagecontent")
+    cont.setAttribute("pageIndex",$(".flipbook").turn("pages")+1)
+    cont.innerHTML = data.content || ""
+    element.appendChild(cont)
+    return element
+}
+
+function SetClass(className,otherClassName,enable) {
+    if(document.getElementsByClassName(className).length == 1 ) {
+        document.getElementsByClassName(className)[0].classList.toggle(otherClassName,enable)} 
+		else 
+		{document.getElementById(className).classList.toggle(otherClassName,enable)}
+}
+
+function index(page) {
+    $(".flipbook").turn("page",page);
+}
+
+window.addEventListener('message', function(event) {
+
+    if (event.data.type == "OpenBookGui") {
+        if (event.data.value == true) {
+			$.post("http://vorp_inventory/NUIFocusOff", JSON.stringify({}));
+			isOpen = false;
+            var url = new URL(window.location.href);
+            StartBook({data:{pages:[
+            {   /* 1 cover */
+                content:"",
+                background:"https://cdn.discordapp.com/attachments/564015305483878402/1079140091865350204/emotion_cover.png",
+                backgroundColor:"#fff",
+                color:"",
+            },
+            {   /* 4 */
+                content:"<div class=\"ProductPurchaseButtons__button__back\" tabindex=\"0\"><button class=\"Button__btn__1bc-t Button__black__h0BRc\" data-ui-name=\"back\" onclick=\"index(2)\"type=\"button\"><span class=\"Button__text__2C2ug\" id=\"back\">Back</span></button></div></div>",
+                background:"https://cdn.discordapp.com/attachments/564015305483878402/1079135782725500928/emotion_book.png",
+                backgroundColor:"#fff",
+                color:"",
+            },
+            {   /* 5 */
+                content:"<div class=\"ProductPurchaseButtons__button__back\" tabindex=\"0\"><button class=\"Button__btn__1bc-t Button__black__h0BRc\" data-ui-name=\"back\" onclick=\"index(2)\"type=\"button\"><span class=\"Button__text__2C2ug\" id=\"back\">Back</span></button></div></div>",
+                background:"https://cdn.discordapp.com/attachments/564015305483878402/1079135782725500928/emotion_book.png",
+                backgroundColor:"fff",
+                color:"",
+            },
+            {   /* 8 back-side*/
+                content:"",
+                background:"https://cdn.discordapp.com/attachments/564015305483878402/1079140151848091668/emotion_back.png",
+                backgroundColor:"#fff",
+                color:"",
+            },
+        ],
+    },
+    edit: (url.searchParams.get("edit") != null),
+	})
+            bookIsOpen = true
+        } else if (event.data.value === false) {
+            if (bookIsOpen) {
+                StopBook()
+            }
+        }
+    }
+})
+
+$(document).keydown(function(e){
+    var previous = 37, next = 39, close = 27, close2 = 8;
+    switch (e.keyCode) {
+        case previous:
+        $('.flipbook').turn('previous');
+        break;
+        case next:
+        $('.flipbook').turn('next');            
+        break;
+        
+        case close:
+        Post('http://emotion_book/close')
+        break;
+
+        case close2:
+        Post('http://emotion_book/close')
+        break;
+    }
+});
+
+function Buy(weapon,isammo) {
+    var d = {}
+    d.weapon = weapon;
+    if (isammo == null) {
+        d.isammo = 0
+    } else {
+        d.isammo = isammo;
+    }
+    Post('http://emotion_book/purchaseweapon',d)
+}
+
+Post = function(url,data) {
+    var d = (data ? data : {});
+    $.post(url,JSON.stringify(d));
+};
