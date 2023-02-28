8ef63c79bc3d594378958bc5a17cdf43863d2e62
Emotion <111930499+Emotion06@users.noreply.github.com>
2023-02-28 17:10:59 +0100

    Delete server.lua

--- server.lua
+++
@@ -1,6 +0,0 @@
-VORP = exports.vorp_inventory:vorp_inventoryApi()
-
-VORP.RegisterUsableItem("book", function(data)
-		--VORP.subItem(data.source, "book", 1)
-		TriggerClientEvent('emotion_book:open', data.source)
-end)
